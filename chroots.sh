3573c891412a50fc9c25b8e6a73221c4e395d9e6
sky line <32512284+linesky@users.noreply.github.com>
2024-06-26 18:25:30 +0100

    Delete chroots.sh

--- chroots.sh
+++
@@ -1,9 +0,0 @@
-printf "\x1bc\x1b[43;37m"
-roots=/mnt/isos
-tmps=/tmp/lists.txt
-tmps2=/tmp/lists2.txt
-sudo umount $roots 2> /dev/null
-sudo mount /tmp/my.img $roots -o loop -t vfat 
-sudo chroot $roots /usr/bin/bash
-sudo chroot / /usr/bin/bash
-sudo umount $roots
